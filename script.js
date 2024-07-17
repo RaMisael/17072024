9d476aa982f6d4cfac4fe1cdb552c55de7c6f164
RaMisa <57572987+RaMisael@users.noreply.github.com>
2024-07-17 06:27:01 +0000

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ noButton.addEventListener("click", function () {
     noCount++;
     const imageIndex = Math.min(noCount, MAX_IMAGES);
     changeImage(imageIndex);
-    resizeYesButton();
     updateNoButtonText();
     if (noCount === MAX_IMAGES) {
       MAX_IMAGES = 0;
@@ -32,24 +31,11 @@ function handleYesClick() {
   changeImage("yes");
 }
 
-function resizeYesButton() {
-  const computedStyle = window.getComputedStyle(yesButton);
-  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
-
-  yesButton.style.fontSize = `${newFontSize}px`;
-}
-
 function generateMessage(noCount) {
   const messages = [
     "No",
     "¿Segura?",
     "Enserio? :c",
-    "No puede ser...",
-    "pipipi :c",
-    "🥺",
-    "pq le sigues picando a No? jsjs",
-    "ya picale a Si jsjsj",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
